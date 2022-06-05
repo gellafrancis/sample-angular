77dbe1cc4063caf885867df1f07dc598c5c9ade5
Francis Gabriel T. Gella II <gella.francis@gmail.com>
2022-06-06 05:50:47 +0800

    [Feature-1] - Deliver 4 functionalities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChallengeOneComponent } from './features/challenge-one/challenge-one.component';
+import { ChallengeTwoComponent } from './features/challenge-two/challenge-two.component';
+import { ChallengeThreeComponent } from './features/challenge-three/challenge-three.component';
+import { ChallengeFourComponent } from './features/challenge-four/challenge-four.component';
+
+const routes: Routes = [
+  { path: '',   redirectTo: '/first-challenge', pathMatch: 'full' },
+  { path: 'first-challenge', component: ChallengeOneComponent },
+  { path: 'second-challenge', component: ChallengeTwoComponent },
+  { path: 'third-challenge', component: ChallengeThreeComponent },
+  { path: 'fourth-challenge', component: ChallengeFourComponent },
+  { path: '**',  redirectTo: '/first-challenge', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ChallengeOneComponent } from './features/challenge-one/challenge-one.component';
+import { ChallengeTwoComponent } from './features/challenge-two/challenge-two.component';
+import { ChallengeThreeComponent } from './features/challenge-three/challenge-three.component';
+import { ChallengeFourComponent } from './features/challenge-four/challenge-four.component';
+import { ChallengeOneUtilityComponent } from './features/challenge-one-utility/challenge-one-utility.component';
+import { ChallengeTwoUtilityComponent } from './features/challenge-two-utility/challenge-two-utility.component';
+import { ChallengeThreeUtilityComponent } from './features/challenge-three-utility/challenge-three-utility.component';
+import { DisplayfoodPipe } from './shared/displayfood.pipe';
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChallengeOneComponent,
+    ChallengeTwoComponent,
+    ChallengeThreeComponent,
+    ChallengeFourComponent,
+    ChallengeOneUtilityComponent,
+    ChallengeTwoUtilityComponent,
+    ChallengeThreeUtilityComponent,
+    DisplayfoodPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
